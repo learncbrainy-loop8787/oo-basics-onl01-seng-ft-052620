71b198b17478df72174d863439c6f5d91fa4be69
korey ko <koreykendrick1@gmail.com>
2020-06-03 16:53:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,8 +5,22 @@ end
 def title
   @title
 end 
-def author=
+def author=(author)
+  @author = author
+end
+def author 
   @author 
-
+end 
+def page_count=(page_count)
+  @page_count = page_count
+end 
+def page_count 
+  @page_count 
+end
+def genre=(genre)
+  @genre = genre 
+end 
+def genre 
+  @genre 
 end
 end
