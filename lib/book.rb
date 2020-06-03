7bfa70494c8fca5d2e2a44ee1c8d8132a1da1f39
korey ko <koreykendrick1@gmail.com>
2020-06-03 16:50:00 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,11 @@
 class Book 
-  def initialize(title, author )
+  def initialize(title)
     @title = title
-    @author = author
 end
 def title
   @title
 end 
-def author 
+def author=
   @author 
 
 end
